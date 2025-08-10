c24a074a0cb061815522835f7ccd8cc0da39b996
Jose Leandro <joseleand06@gmail.com>
2025-08-10 20:25:57 -0300

    2 - Criação do pacote Model e classe Transaction

---
+++ src/main/java/Projeto01/Transactions_API/model/Transaction.java
@@ -0,0 +1,21 @@
+package Projeto01.Transactions_API.model;
+
+import java.time.OffsetDateTime;
+
+public class Transaction {
+    private Double valor;
+    private OffsetDateTime dataHora;
+
+    public Transaction(Double valor, OffsetDateTime dataHora){
+        this.valor = valor;
+        this.dataHora = dataHora;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public OffsetDateTime getDataHora() {
+        return dataHora;
+    }
+}

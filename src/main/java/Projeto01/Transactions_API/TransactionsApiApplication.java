9138f84d9397aa7b94b747b4efb3d3c6ac6fc2d1
Jose Leandro <joseleand06@gmail.com>
2025-08-10 20:14:48 -0300

    1 - Importando arquivos e dependências do SpringBoot

---
+++ src/main/java/Projeto01/Transactions_API/TransactionsApiApplication.java
@@ -0,0 +1,13 @@
+package Projeto01.Transactions_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Projeto01/Transactions_API/TransactionsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Projeto01.Transactions_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

9acf34af84d1bafd0a22aa7f30fa7a1b9feb72a8
Jose Leandro <joseleand06@gmail.com>
2025-08-10 22:01:03 -0300

    5 - Criação do pacote Service e classe TransactionService

---
+++ src/main/java/Projeto01/Transactions_API/service/TransactionService.java
@@ -0,0 +1,31 @@
+package Projeto01.Transactions_API.service;
+
+import Projeto01.Transactions_API.model.Transaction;
+import org.springframework.stereotype.Service;
+
+import java.time.OffsetDateTime;
+import java.util.DoubleSummaryStatistics;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+@Service
+public class TransactionService {
+    private final Queue<Transaction> transactions = new ConcurrentLinkedQueue<>();
+
+    public void addTransaction(Transaction transaction) {
+        transactions.add(transaction);
+    }
+
+    public void clearTransaction() {
+        transactions.clear();
+    }
+
+    public DoubleSummaryStatistics summaryStatistics() {
+        OffsetDateTime now = OffsetDateTime.now();
+        return transactions.stream()
+                .filter(t -> t.getDataHora().isAfter(now.minusSeconds(60)))
+                .mapToDouble(Transaction::getValor)
+                .summaryStatistics();
+    }
+}
